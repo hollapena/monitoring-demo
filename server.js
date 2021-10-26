dd1eadb0977bfd7403c08cb6140111cd02adb10c
Callen Schultz <callen.schultz@gmail.com>
2021-10-26 13:38:05 -0500

    Added dynamic error handling

--- server.js
+++ server.js
@@ -19,14 +19,28 @@ let students = []
 app.post('/api/student', (req,res) => {
     let {name} = req.body
     name = name.trim()
-    students.push(name)
-    rollbar.log('student added successfully', {author: 'Callie', type: 'manual entry'})
-    res.status(200).send(students)
+
+    const index = students.findIndex(studentName => studentName === name)
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('student added successfully', {author: 'Callie', type: 'manual entry'})
+        res.status(200).send(students)
+    }else if(name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('Must provide a name.')
+    }else{
+        rollbar.critical('Student already exists')
+        res.status(400).send('That student already exists')
+    }
 })
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully')
+})
+
+app.get('/style', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/public/styles.css'))
 })
 
 // Placement is important for the error handler...make sure it is below the body, but above the listener
