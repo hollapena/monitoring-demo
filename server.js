6ff77d325801945993caf3b2da369b13b483df52
Callen Schultz <callen.schultz@gmail.com>
2021-10-26 12:38:40 -0500

    Added Rollbar

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
-    rollbar.info('html fil served successfully')
+    rollbar.info('html file served successfully')
 })
 
 const port = process.env.PORT || 4545
