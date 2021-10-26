f7bdac9a4b3326e50999caab221eeadebf6744fb
Callen Schultz <callen.schultz@gmail.com>
2021-10-26 13:14:07 -0500

    corrections

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require('path')
 
 const Rollbar = require('rollbar')
 
+const app = express()
+app.use(express.json())
+
+
 let rollbar = new Rollbar({
     accessToken: '072b2e6a1ff348d9af0aba716638d4c7',
     captureUncaught: true,
@@ -19,8 +23,6 @@ app.post('/api/student', (req,res) => {
     rollbar.log('student added successfully', {author: 'Callie', type: 'manual entry'})
     res.status(200).send(students)
 })
-
-const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
